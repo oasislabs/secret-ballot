7f6fbad25f5e27da26fc95586d3e5133ad7fc253
Jernej Kos <jernej@kos.mx>
2018-11-14 12:57:00 +0100

    migrations: Wrap candidate names using web3.utils.fromAscii

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,8 @@
 var SecretBallot = artifacts.require("SecretBallot");
 module.exports = function(deployer) {
-  deployer.deploy(SecretBallot, ['John', 'Jeff', 'Jim'])
+    deployer.deploy(SecretBallot, [
+        web3.utils.fromAscii('John'),
+        web3.utils.fromAscii('Jeff'),
+        web3.utils.fromAscii('Jim'),
+    ])
 };
