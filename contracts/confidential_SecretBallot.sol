ac56356d3b7eccfcc21092b7171075790486ec49
Peter Us <ptrusr@gmail.com>
2018-11-14 16:30:34 +0100

    solidity: 4 spaces convention

--- contracts/confidential_SecretBallot.sol
+++ contracts/confidential_SecretBallot.sol
@@ -1,64 +1,61 @@
 pragma solidity ^0.4.18;
 
 contract SecretBallot {
-  // The address of the account that created this ballot.
-  address public ballotCreator;
+    // The address of the account that created this ballot.
+    address public ballotCreator;
 
-  // Is voting finished? The ballot creator determines when to set this flag.
-  bool public votingEnded;
-  
-  // Candidate names
-  bytes32[] public candidateNames;
+    // Is voting finished? The ballot creator determines when to set this flag.
+    bool public votingEnded;
 
-  // Keep track of which addresses have voted already to prevent multiple votes.
-  mapping (address => bool) public hasVoted;
-  
-  // Tallies for each candidate
-  mapping (bytes32 => uint16) private votesReceived;
+    // Candidate names
+    bytes32[] public candidateNames;
 
-  // The total number of votes cast so far. Revealed before voting has ended.
-  uint16 public totalVotes;
+    // Keep track of which addresses have voted already to prevent multiple votes.
+    mapping (address => bool) public hasVoted;
 
-  constructor(bytes32[] _candidateNames) public {
-    ballotCreator = msg.sender;
-    candidateNames = _candidateNames;
-  }
-  
-  function voteForCandidate(bytes32 candidate) public {
-    require(!votingEnded);
-    require(validCandidate(candidate));
-    require(!hasVoted[msg.sender]);
+    // Tallies for each candidate
+    mapping (bytes32 => uint16) private votesReceived;
 
-    votesReceived[candidate] += 1;
-    hasVoted[msg.sender] = true;
-    totalVotes += 1;
-  }
+    // The total number of votes cast so far. Revealed before voting has ended.
+    uint16 public totalVotes;
 
-  function endVoting() public returns (bool) {
-    require(msg.sender == ballotCreator);  // Only ballot creator can end the vote.
-    votingEnded = true;
-    return true;
-  }
-  
-  function totalVotesFor(bytes32 candidate) view public returns (uint16) {
-    require(validCandidate(candidate));
-    require(votingEnded);  // Don't reveal votes until voting has ended
-    return votesReceived[candidate];
-  }
-
-  function numCandidates() public constant returns(uint count) {
-    return candidateNames.length;
-  }
-
-  function validCandidate(bytes32 candidate) view public returns (bool) {
-    for(uint i = 0; i < candidateNames.length; i++) {
-      if (candidateNames[i] == candidate) {
-        return true;
-      }
+    constructor(bytes32[] _candidateNames) public {
+        ballotCreator = msg.sender;
+        candidateNames = _candidateNames;
     }
-    return false;
-  }
 
-  
+    function voteForCandidate(bytes32 candidate) public {
+        require(!votingEnded);
+        require(validCandidate(candidate));
+        require(!hasVoted[msg.sender]);
 
+        votesReceived[candidate] += 1;
+        hasVoted[msg.sender] = true;
+        totalVotes += 1;
+    }
+
+    function endVoting() public returns (bool) {
+        require(msg.sender == ballotCreator);  // Only ballot creator can end the vote.
+        votingEnded = true;
+        return true;
+    }
+
+    function totalVotesFor(bytes32 candidate) view public returns (uint16) {
+        require(validCandidate(candidate));
+        require(votingEnded);  // Don't reveal votes until voting has ended
+        return votesReceived[candidate];
+    }
+
+    function numCandidates() public constant returns(uint count) {
+        return candidateNames.length;
+    }
+
+    function validCandidate(bytes32 candidate) view public returns (bool) {
+        for(uint i = 0; i < candidateNames.length; i++) {
+            if (candidateNames[i] == candidate) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
