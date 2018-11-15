3b201fe8748f5301641e678ee6ce5d8dceddca43
Armani Ferrante <armaniferrante@gmail.com>
2018-11-15 00:56:43 +0000

    Spacing

--- contracts/confidential_SecretBallot.sol
+++ contracts/confidential_SecretBallot.sol
@@ -33,7 +33,7 @@ contract SecretBallot {
         require(!hasVoted[msg.sender]);
         // prevent overflow
         require(votesReceived[candidate] < ~uint256(0));
-		require(totalVotes < ~uint256(0));
+        require(totalVotes < ~uint256(0));
 
         votesReceived[candidate] += 1;
         hasVoted[msg.sender] = true;
